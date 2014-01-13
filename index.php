e7c4960a1a6427b81bf0abe8f66f8dc95877d21f
moises lopes ferreira <judeudemolidor@hotmail.com>
2014-01-13 14:16:30 -0200

    alteração layout issue #4
    alinhamento brand social

--- index.php
+++ index.php
@@ -67,7 +67,8 @@
                 </div>
                 <?php } ?>
               </div><!-- /.row -->
-              <div class="row center-block">
+              <BR>
+              <div class="row">
                 <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
                   <ul class="list-inline acompanhe ">
                     <li>
