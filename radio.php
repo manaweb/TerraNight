62e2766249038e30670edc7d83871afeba0f15ea
Moises lopes ferreira <seliqueti@gmail.com>
2014-01-28 10:03:44 -0200

    Pop-up Radio Issue #22
    Adicionado tela Radio terranight
    ajustes de extensão

---
+++ radio.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7">
+  <![endif]-->
+  <!--[if IE 7]>
+  <html class="no-js lt-ie9 lt-ie8">
+    <![endif]-->
+    <!--[if IE 8]>
+    <html class="no-js lt-ie9">
+      <![endif]-->
+      <!--[if gt IE 8]>
+      <!-->
+      <html class="no-js">
+        <!--<![endif]-->
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Boate Terra Night">
+        <meta name="author" content="Boate Terra Night">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/modern-business.css">
+        <link rel="stylesheet" href="fonts/alte_din_1451_mittelschrift.css">
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bullets-slideshow.css">
+
+        <!--[if gte IE 9]>
+        <style type="text/css">
+    .gradient {
+       filter: none;
+    }
+  </style>
+        <![endif]-->
+        <!--[if lt IE 9]>
+        <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+        <![endif]-->
+        <!--[if lt IE 8]>
+        <link href="/css/bootstrap-ie7.css" rel="stylesheet">
+        <![endif]-->
+</head>
+<body onload="window.open('http://terranight.com.br/radio.html', 'Rádio - Terra Night', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=NO, TOP=10, LEFT=10, WIDTH=500, HEIGHT=250')">
+  	<?php include_once("analyticstracking.php") ?>
+
+	<a href="index.php">
+		<img class="img-responsive" src="img/radio.png">
+	</a>
+	<br>
+	<iframe src="http://www.virtualcast.com.br/players/audio/002/transparente.php?canal=terranight&autoplay=1" allowtransparency="true" frameborder="0" height="38" scrolling="No" width="450"></iframe>
+  </body>
+</html>
